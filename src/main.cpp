92845cc92dc7fb4482e6972bdb8da2281baf7967
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-06-04 16:08:06 +0200

    add simple networking poc code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
+#include <iostream>
+
 #include <yarrr/ship.hpp>
+#include <yarrr/network.hpp>
+
+class TestMessageFactory
+{
+  public:
+    static void received_data()
+    {
+      std::cout << "data is received" << std::endl;
+    }
+};
 
 int main( int argc, char ** argv )
 {
   yarrr::Ship ship;
   yarrr::time_step( ship );
+
+  yarrr::SocketPool<TestMessageFactory> pool( 2000 );
+  pool.listen();
+  pool.start();
   return 0;
 }
 
