2ca0c21ac407c0fcc1a35de10f3ac819890674de
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-06-06 10:57:24 +0200

    add callback functions to handle new connection and data ready events

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,28 @@
 #include <yarrr/ship.hpp>
 #include <yarrr/network.hpp>
 
+namespace
+{
+  void new_connection( yarrr::Socket& )
+  {
+    std::cout << "new connection established" << std::endl;
+  }
+
+  void data_available_on( yarrr::Socket& socket )
+  {
+    std::cout << "data is available" << std::endl;
+  }
+}
+
 int main( int argc, char ** argv )
 {
   yarrr::Ship ship;
   yarrr::time_step( ship );
 
-  yarrr::SocketPool pool;
+  yarrr::SocketPool pool(
+      new_connection,
+      data_available_on
+      );
   pool.listen( 2000 );
   pool.start();
   return 0;
