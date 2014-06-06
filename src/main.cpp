d8117b3c36c37c3c9a88b720f20d2bd03359dd56
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-06-06 12:18:54 +0200

    move read to a lower layer

--- src/main.cpp
+++ src/main.cpp
@@ -14,21 +14,14 @@ namespace
     std::cout << "new connection established" << std::endl;
   }
 
-  const int max_message_size( 1000 );
-  std::array<char,max_message_size> messageBuffer;
-  void data_available_on( yarrr::Socket& socket )
+  void data_available_on( yarrr::Socket& socket, char* message, size_t length )
   {
-    //todo: move reading and data buffering to lower layer
-    const size_t length( ::read( socket.fd, &messageBuffer[0], max_message_size ) );
-    std::cout << "data arrived: " << std::string( &messageBuffer[0], &messageBuffer[length] ) << std::endl;
+    std::cout << "data arrived: " << std::string( message, length ) << std::endl;
   }
 }
 
 int main( int argc, char ** argv )
 {
-  yarrr::Ship ship;
-  yarrr::time_step( ship );
-
   yarrr::SocketPool pool(
       new_connection,
       data_available_on
