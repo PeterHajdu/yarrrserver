d2c0df136c4709b5fa6d07d41314c50140e82118
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-06-06 10:06:07 +0200

    fix yarrrserver

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,12 @@
 #include <yarrr/ship.hpp>
 #include <yarrr/network.hpp>
 
-class TestMessageFactory
-{
-  public:
-    static void received_data()
-    {
-      std::cout << "data is received" << std::endl;
-    }
-};
-
 int main( int argc, char ** argv )
 {
   yarrr::Ship ship;
   yarrr::time_step( ship );
 
-  yarrr::SocketPool<TestMessageFactory> pool;
+  yarrr::SocketPool pool;
   pool.listen( 2000 );
   pool.start();
   return 0;
