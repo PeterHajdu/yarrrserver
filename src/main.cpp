b70b1cf9aa23849c0bc7ee364202a7b02e39308b
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-06-06 12:49:46 +0200

    add connection loss handling

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,11 @@ namespace
     std::cout << "new connection established" << std::endl;
   }
 
+  void lost_connection( yarrr::Socket& )
+  {
+    std::cout << "connection lost" << std::endl;
+  }
+
   void data_available_on( yarrr::Socket& socket, char* message, size_t length )
   {
     std::cout << "data arrived: " << std::string( message, length ) << std::endl;
@@ -24,8 +29,8 @@ int main( int argc, char ** argv )
 {
   yarrr::SocketPool pool(
       new_connection,
-      data_available_on
-      );
+      lost_connection,
+      data_available_on );
   pool.listen( 2000 );
   pool.start();
   return 0;
