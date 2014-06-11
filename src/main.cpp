08052802a8c691976f53e30b157ec14558a8496b
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-06-11 11:31:48 +0200

    add thenet dependency

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,17 @@
 #include <mutex>
 
 #include <yarrr/ship.hpp>
-#include <yarrr/socket_pool.hpp>
+#include <thenet/socket_pool.hpp>
 
 
 namespace
 {
-  void lost_connection( yarrr::Socket& )
+  void lost_connection( the::net::Socket& )
   {
     std::cout << "connection lost" << std::endl;
   }
 
-  void data_available_on( yarrr::Socket& socket, const char* message, size_t length )
+  void data_available_on( the::net::Socket& socket, const char* message, size_t length )
   {
     std::cout << "data arrived: " << std::string( message, length ) << std::endl;
   }
@@ -25,7 +25,7 @@ namespace
 class Player
 {
   public:
-    Player( yarrr::Socket& socket )
+    Player( the::net::Socket& socket )
       : m_socket( socket )
       , m_ship( "Ship: 1000 1100 1 2 3000 3" )
     {
@@ -39,7 +39,7 @@ class Player
     }
 
   private:
-    yarrr::Socket& m_socket;
+    the::net::Socket& m_socket;
     yarrr::Ship m_ship;
 };
 
@@ -49,8 +49,8 @@ int main( int argc, char ** argv )
   std::vector< Player > players;
   std::mutex players_mutex;
 
-  yarrr::SocketPool pool(
-      [ &players, &players_mutex ]( yarrr::Socket& socket )
+  the::net::SocketPool pool(
+      [ &players, &players_mutex ]( the::net::Socket& socket )
       {
         std::lock_guard<std::mutex> lock( players_mutex );
         players.emplace_back( socket );
@@ -59,7 +59,7 @@ int main( int argc, char ** argv )
       data_available_on );
   pool.listen( 2000 );
 
-  std::thread network_thread( std::bind( &yarrr::SocketPool::start, &pool ) );
+  std::thread network_thread( std::bind( &the::net::SocketPool::start, &pool ) );
 
   while ( true )
   {
