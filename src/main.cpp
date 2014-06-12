7a6757678ae7299cd37692e5cbc4fdfd6197ec8d
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-06-12 12:40:00 +0200

    add messagequeue to player class

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 
 #include <yarrr/ship.hpp>
 #include <thenet/socket_pool.hpp>
+#include <thenet/message_queue.hpp>
 
 
 namespace
@@ -25,8 +26,15 @@ namespace
 class Player
 {
   public:
+    typedef std::unique_ptr< Player > Pointer;
+
     Player( the::net::Socket& socket )
       : m_socket( socket )
+      , m_message_queue(
+          std::bind(
+            &the::net::Socket::send,
+            &m_socket,
+            std::placeholders::_1, std::placeholders::_2 ) )
       , m_ship( "Ship: 1000 1100 1 2 3000 3" )
     {
     }
@@ -35,31 +43,40 @@ class Player
     {
       yarrr::time_step( m_ship );
       const std::string ship_data( yarrr::serialize( m_ship ) );
-      m_socket.send( ship_data.data(), ship_data.size() );
+      m_message_queue.send( { begin( ship_data ), end( ship_data ) } );
     }
 
   private:
     the::net::Socket& m_socket;
+    the::net::MessageQueue m_message_queue;
     yarrr::Ship m_ship;
 };
 
 int main( int argc, char ** argv )
 {
 
-  std::vector< Player > players;
+  std::vector< Player::Pointer > players;
   std::mutex players_mutex;
 
   the::net::SocketPool pool(
       [ &players, &players_mutex ]( the::net::Socket& socket )
       {
         std::lock_guard<std::mutex> lock( players_mutex );
-        players.emplace_back( socket );
+        players.emplace_back( new Player( socket ) );
       },
       lost_connection,
       data_available_on );
   pool.listen( 2000 );
 
-  std::thread network_thread( std::bind( &the::net::SocketPool::start, &pool ) );
+  std::thread network_thread(
+      [ &pool ]()
+      {
+        while ( true )
+        {
+          const int ten_milliseconds( 10 );
+          pool.run_for( ten_milliseconds );
+        }
+      } );
 
   while ( true )
   {
@@ -67,14 +84,13 @@ int main( int argc, char ** argv )
       std::lock_guard<std::mutex> lock( players_mutex );
       for ( auto& player : players )
       {
-        player.update();
+        player->update();
       }
     }
 
     std::this_thread::sleep_for( std::chrono::seconds( 1 ) );
   }
 
-  pool.stop();
   network_thread.join();
   return 0;
 }
