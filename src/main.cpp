ea9c26baa5dcba48afaec5c3f0e28704895243f1
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-06-16 17:53:56 +0200

    move bunch of code to thenet

--- src/main.cpp
+++ src/main.cpp
@@ -8,126 +8,7 @@
 
 #include <yarrr/ship.hpp>
 #include <thenet/socket_pool.hpp>
-#include <thenet/message_queue.hpp>
-#include <thenet/message_buffer.hpp>
-
-namespace
-{
-  class DummyParser
-  {
-    public:
-      static size_t parse( const char*, size_t length )
-      {
-        return length;
-      }
-  };
-}
-
-class Connection
-{
-  public:
-    typedef std::unique_ptr<Connection> Pointer;
-    Connection( the::net::Socket& socket )
-      : id( socket.id )
-      , m_socket( socket )
-      , m_message_queue(
-          std::bind(
-            &the::net::Socket::send,
-            &m_socket,
-            std::placeholders::_1, std::placeholders::_2 ) )
-      , m_incoming_buffer( m_message_queue )
-    {
-    }
-
-    bool send( the::net::Data&& message )
-    {
-      return m_message_queue.send( std::move( message ) );
-    }
-
-    bool receive( the::net::Data& message )
-    {
-      return m_message_queue.receive( message );
-    }
-
-    void wake_up_on_network_thread()
-    {
-      m_message_queue.wake_up();
-    }
-
-    void data_from_network( const char* data, size_t length )
-    {
-      m_incoming_buffer.receive( data, length );
-    }
-
-    const int id;
-  private:
-    the::net::Socket& m_socket;
-    the::net::MessageQueue m_message_queue;
-    the::net::MessageBuffer<DummyParser,the::net::MessageQueue> m_incoming_buffer;
-};
-
-
-class ConnectionPool
-{
-  public:
-    typedef std::function< void(Connection&) > ConnectionCallback;
-    ConnectionPool(
-        ConnectionCallback new_connection,
-        ConnectionCallback lost_connection )
-      : m_new_connection( new_connection )
-      , m_lost_connection( lost_connection )
-    {
-    }
-
-    void on_new_socket( the::net::Socket& socket )
-    {
-      Connection::Pointer new_connection( new Connection( socket ) );
-      m_new_connection( *new_connection );
-
-      m_connections.emplace( std::make_pair(
-            socket.id,
-            std::move( new_connection ) ) );
-    }
-
-    void on_lost_socket( the::net::Socket& socket )
-    {
-      ConnectionContainer::iterator connection( m_connections.find( socket.id ) );
-      if ( connection == m_connections.end() )
-      {
-        return;
-      }
-
-      m_lost_connection( *connection->second );
-      m_connections.erase( connection );
-    }
-
-    void on_data_arrived( the::net::Socket& socket, const char* message, size_t length )
-    {
-      ConnectionContainer::iterator connection( m_connections.find( socket.id ) );
-      if ( connection == m_connections.end() )
-      {
-        return;
-      }
-
-      connection->second->data_from_network( message, length );
-    }
-
-
-    void wake_up_on_network_thread()
-    {
-      for ( auto& connection : m_connections )
-      {
-        connection.second->wake_up_on_network_thread();
-      }
-    }
-
-  private:
-    typedef std::unordered_map< int, Connection::Pointer > ConnectionContainer;
-    ConnectionContainer m_connections;
-    ConnectionCallback m_new_connection;
-    ConnectionCallback m_lost_connection;
-};
-
+#include <thenet/connection_pool.hpp>
 
 class Player
 {
@@ -161,35 +42,25 @@ int main( int argc, char ** argv )
   std::unordered_map< int, Player::Pointer > players;
   std::mutex players_mutex;
 
-  std::vector< std::reference_wrapper< Connection > > connections;
-
-  ConnectionPool cpool(
-      [ &connections, &players, &players_mutex ]( Connection& connection )
+  the::net::ConnectionPool cpool(
+      [ &players, &players_mutex ]( the::net::Connection& connection )
       {
         std::lock_guard<std::mutex> lock( players_mutex );
         Player::Pointer new_player( new Player( connection.id ) );
         players.emplace( std::make_pair(
           connection.id,
           std::move( new_player ) ) );
-        connections.emplace_back( connection );
       },
-      [ &connections, &players, &players_mutex ]( Connection& connection )
+      [ &players, &players_mutex ]( the::net::Connection& connection )
       {
         std::lock_guard<std::mutex> lock( players_mutex );
         players.erase( connection.id );
-        connections.erase(
-          std::remove_if( begin( connections ), end( connections ),
-          [ &connection ]( std::reference_wrapper< Connection >& element )
-          {
-            return connection.id == element.get().id;
-          } ),
-          std::end( connections ) );
       } );
 
   the::net::SocketPool pool(
-      std::bind( &ConnectionPool::on_new_socket, &cpool, std::placeholders::_1 ),
-      std::bind( &ConnectionPool::on_lost_socket, &cpool, std::placeholders::_1 ),
-      std::bind( &ConnectionPool::on_data_arrived, &cpool,
+      std::bind( &the::net::ConnectionPool::on_new_socket, &cpool, std::placeholders::_1 ),
+      std::bind( &the::net::ConnectionPool::on_socket_lost, &cpool, std::placeholders::_1 ),
+      std::bind( &the::net::ConnectionPool::on_data_available, &cpool,
         std::placeholders::_1, std::placeholders::_2, std::placeholders::_3 ) );
 
   pool.listen( 2000 );
@@ -210,30 +81,31 @@ int main( int argc, char ** argv )
 
   while ( true )
   {
+    std::vector< the::net::Data > ship_states;
     {
       std::lock_guard<std::mutex> lock( players_mutex );
-      std::vector< the::net::Data > ship_states;
       for ( auto& player : players )
       {
         player.second->update();
         ship_states.emplace_back( player.second->serialize() );
       }
-
-      for ( auto& connection : connections )
-      {
-        for ( auto ship_state : ship_states )
-        {
-          connection.get().send( the::net::Data( ship_state ) );
-        }
-
-        the::net::Data message;
-        while ( connection.get().receive( message ) )
-        {
-          std::cout << "message arrived from player: " << connection.get().id << " " <<
-            std::string( begin( message ), end( message ) ) << std::endl;
-        }
-      }
     }
+
+    cpool.enumerate(
+        [ &ship_states ]( the::net::Connection& connection )
+        {
+          for ( auto& ship_state : ship_states )
+          {
+            connection.send( the::net::Data( ship_state ) );
+          }
+
+          the::net::Data message;
+          while ( connection.receive( message ) )
+          {
+            std::cout << "message arrived from player: " << connection.id << " " <<
+            std::string( begin( message ), end( message ) ) << std::endl;
+          }
+        } );
 
     std::this_thread::sleep_for( std::chrono::milliseconds( 100 ) );
   }
