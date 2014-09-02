05aa9c231ea961999a066e39963a7ee4c47f831b
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-09-02 12:32:03 +0200

    add missing service in unittests

--- test/test_services.cpp
+++ test/test_services.cpp
@@ -1,9 +1,14 @@
-#include <thectci/service_registry.hpp>
 #include "../src/local_event_dispatcher.hpp"
+#include <yarrr/engine_dispatcher.hpp>
+
+#include <thectci/service_registry.hpp>
 
 namespace
 {
   the::ctci::AutoServiceRegister< LocalEventDispatcher, LocalEventDispatcher >
     local_event_dispatcher_register;
+
+  the::ctci::AutoServiceRegister< yarrr::EngineDispatcher, yarrr::EngineDispatcher >
+    auto_engine_dispatcher_register;
 }
 
