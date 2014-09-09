5d9c44a2e70dc3682f466917bf405053a3413f71
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-09-09 14:53:42 +0200

    add watchr config file

---
+++ watchr_config.rb
@@ -0,0 +1,24 @@
+require 'libnotify'
+
+def failure( command )
+  Libnotify.show( :body => command, :urgency => :critical, :summary => "failure", :timeout => 2.5 )
+  false
+end
+
+def success( command )
+  Libnotify.show( :body => command, :urgency => :low, :summary => "success", :timeout => 2.5 )
+  true
+end
+
+def run( command )
+  if ( system( command ) )
+    success( command )
+  else
+    failure( command )
+  end
+end
+
+watch( '.*pp' ) do | filename |
+  run( "compile" ) && run( "unittest" ) && run( "deploy" ) && run( "cucumber" )
+end
+
