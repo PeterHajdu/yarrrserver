8b0fd93183b1dbe2a2ffb4cdd1b12c666df592da
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-10-13 15:54:07 +0200

    players test cleanup

--- test/test_player.cpp
+++ test/test_player.cpp
@@ -46,6 +46,7 @@ Describe( a_player )
     player->assign_object( *ship );
     AssertThat( player->object_id(), Equals( ship->id ) );
     AssertThat( connection->has_no_data(), Equals( false ) );
+    AssertThat( connection->get_entity< yarrr::ObjectAssigned >()->object_id(), Equals( ship->id ) );
   }
 
   It ( forwards_commands_from_the_network_to_the_object )
