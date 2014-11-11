9735329da6d6579f3948b94122e17055c659b995
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-11-11 12:27:43 +0100

    add mission factory

--- src/services.cpp
+++ src/services.cpp
@@ -1,6 +1,7 @@
 #include <yarrr/engine_dispatcher.hpp>
 #include <yarrr/dummy_graphical_engine.hpp>
 #include <yarrr/main_thread_callback_queue.hpp>
+#include <yarrr/mission_factory.hpp>
 #include <yarrr/object_factory.hpp>
 #include <thectci/service_registry.hpp>
 
@@ -9,6 +10,9 @@
 
 namespace
 {
+  the::ctci::AutoServiceRegister< yarrr::MissionFactory, yarrr::MissionFactory >
+    mission_factory_register;
+
   the::ctci::AutoServiceRegister< yarrr::ObjectFactory, yarrr::ObjectFactory >
     object_factory_register;
 
