e91d9dbe57f8ce679c72f4039eb0ce22cd1e63bb
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2015-03-25 12:58:17 +0100

    use the-model-remote instead of silly zmq ruby modules that are impossible to install

--- features/support/model_extractor.rb
+++ features/support/model_extractor.rb
@@ -1,19 +1,12 @@
-require 'zmq'
-
 class ModelExtractor
 
   def initialize( endpoint )
     @endpoint = endpoint
-    @context = ZMQ::Context.new
-    @socket = @context.socket( :REQ )
-    @socket.connect @endpoint
-    @socket.rcvtimeo = 3000
   end
 
   def extract_model
-    @socket.send ""
-    response = @socket.recv
-    if response == nil then
+    response = `the-model-remote #{@endpoint}`
+    if not $?.success? then
       raise "Unable to extract model."
     end
 
